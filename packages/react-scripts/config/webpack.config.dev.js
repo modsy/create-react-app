454b84950256dd1bf38a0f4124a18c697d5ad7e4
Mike Chau <mike@chau.io>
2017-08-23 11:40:39 -0700

    Switch to inline-source-map for now

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -63,7 +63,7 @@ var entryPoints = webpackConfigHelper.buildEntryPointConfig({
 module.exports = {
   // You may want 'eval' instead if you prefer to see the compiled output in DevTools.
   // See the discussion in https://github.com/facebookincubator/create-react-app/issues/343.
-  devtool: 'cheap-module-source-map',
+  devtool: 'inline-source-map',
   // These are the "entry points" to our application.
   // This means they will be the "root" imports that are included in JS bundle.
   // The first two entry points enable "hot" CSS and auto-refreshes for JS.
